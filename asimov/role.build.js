53d36a723bfbf386279c1590d0ba3e3689341c7e
thykka <thykka@gmail.com>
2019-05-24 02:05:58 +0300

    :)

--- asimov/role.build.js
+++ asimov/role.build.js
@@ -11,7 +11,7 @@ const RoleBuild = {
     }
 
     if(creep.memory.building) {
-      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+      var targets = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
       if(targets.length) {
         if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
           creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ff8800'}});
@@ -19,7 +19,7 @@ const RoleBuild = {
       }
     }
     else {
-      var sources = creep.room.find(FIND_SOURCES);
+      var sources = creep.room.find(FIND_SOURCES_ACTIVE);
       if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
         creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ff0088'}});
       }
