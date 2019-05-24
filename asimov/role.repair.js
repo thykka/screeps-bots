3c3ac332040d244c191c1f86b2b298b925b30c84
thykka <thykka@gmail.com>
2019-05-24 21:41:47 +0300

    repairers should harvest

--- asimov/role.repair.js
+++ asimov/role.repair.js
@@ -13,22 +13,34 @@ const RoleRepair = {
     );
   },
   run: (creep) => {
-    // It should find the nearest flag, move to it, dismantle it, repeat
-    /*
-    const target = creep.pos.findClosestByRange(FIND_STRUCTURES,
-      {filter: {structureType: STRUCTURE_WALL}});
-      */
-    const flag = Object.values(Game.flags)[0];
-    console.log('flag: ' + JSON.stringify(flag ? flag.pos : flag));
-    const target = flag ? creep.room.find(FIND_MY_STRUCTURES, {
-      filter: object => object.hits < object.hitsMax
-    }) : false;
-    console.log('target: ' + JSON.stringify(target));
-    if(target) {
-      const result = creep.repair(target);
-      if(result == ERR_NOT_IN_RANGE) {
-        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-      } else { console.log('! repair failed: ' + result + ' Flag: ' + JSON.stringify(target)); }
+    if(creep.memory.repairing && creep.carry.energy == 0) {
+      creep.memory.repairing = false;
+      creep.say('r: harvest');
+    }
+    if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.repairing = true;
+      creep.say('r: repair');
+    }
+
+    if(creep.memory.repairing) {
+      const flag = Object.values(Game.flags)[0];
+      console.log('flag: ' + JSON.stringify(flag ? flag.pos : flag));
+      const target = flag ? creep.room.find(FIND_MY_STRUCTURES, {
+        filter: object => object.hits < object.hitsMax
+      }) : false;
+      console.log('target: ' + JSON.stringify(target));
+      if(target) {
+        const result = creep.repair(target);
+        if(result == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+        } else { console.log('! repair failed: ' + result + ' Flag: ' + JSON.stringify(target)); }
+      }
+    }
+    else {
+      var sources = creep.room.find(FIND_SOURCES_ACTIVE);
+      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ff0088'}});
+      }
     }
   }
 };
