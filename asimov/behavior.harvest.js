816a5bc413f347995aff6aca325d9c42f11a6aea
thykka <thykka@gmail.com>
2019-05-25 16:36:01 +0300

    change things a bit

--- asimov/behavior.harvest.js
+++ asimov/behavior.harvest.js
@@ -1,11 +1,15 @@
-const HarvestBehavior = function(creep, byPath = false) {
+const harvestBehavior = function(creep, byPath = false) {
   let sources = byPath ?
     creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE) :
-    creep.room.find(FIND_SOURCES_ACTIVE);
+    creep.room.find(FIND_SOURCES);
+
+  if(sources[0] === undefined) {
+    console.log('No harvest targets found!');
+  }
 
   if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
     creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffff00'}});
   }
 };
 
-module.exports = HarvestBehavior;
+module.exports = harvestBehavior;
