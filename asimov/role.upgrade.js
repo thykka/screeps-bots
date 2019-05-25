2f5c8c0893f7d596c2516408b2e32abd93cdeaa2
thykka <thykka@gmail.com>
2019-05-25 16:49:11 +0300

    upgrade role comments

--- asimov/role.upgrade.js
+++ asimov/role.upgrade.js
@@ -3,27 +3,26 @@ const harvestBehavior = require('behavior.harvest');
 
 const RoleUpgrade = {
   name: 'upgrade',
-  idealCount: 8,
+  idealCount: 7,
   memory: {
     upgrading: false,
   },
   spawn: (spawner, prefix) => spawnDrone(spawner, RoleUpgrade, [WORK, CARRY, CARRY, MOVE], prefix),
   run: (creep) => {
-
+    // No energy left for upgrading
     if(creep.memory.upgrading && creep.carry.energy == 0) {
       creep.memory.upgrading = false;
     }
+    // Carry full, resume upgrading
     if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
       creep.memory.upgrading = true;
     }
-
-    if(creep.memory.upgrading) {
+    if(!creep.memory.upgrading) {
+      harvestBehavior(creep);
+    } else {
       if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
         creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#0000ff'}});
       }
-    }
-    else {
-      harvestBehavior(creep);
     }
   }
 };
