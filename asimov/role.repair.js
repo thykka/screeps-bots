c0b957df75d9f7682ed115f358acbd07185253fb
thykka <thykka@gmail.com>
2019-05-25 16:06:07 +0300

    improve repair targeting

--- asimov/role.repair.js
+++ asimov/role.repair.js
@@ -8,41 +8,42 @@ const RoleRepair = {
   },
   spawn: (spawner, prefix) => spawnDrone(spawner, RoleRepair, [WORK, CARRY, CARRY, MOVE], prefix),
   run: (creep) => {
+    // Switch to getting energy
     if(creep.memory.repairing && creep.carry.energy == 0) {
       creep.memory.repairing = false;
     }
+
+    // Switch to repairing
     if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
       creep.memory.repairing = true;
+
+      let target = creep.room.find(FIND_MY_STRUCTURES)
+        .filter(o => o.hits < o.hitsMax)
+        .sort((a, b) => a.hits - b.hits);
+
+      íf(target.length === 0) {
+        target = creep.room.find(FIND_STRUCTURES)
+          .filter(o => ((
+            o instanceof StructureWall ||
+            o instanceof StructureRampart ||
+            o instanceof StructureTower ||
+            o instanceof StructureRoad
+          ) && o.hits < o.hitsMax ))
+          .sort((a, b) => a.hits - b.hits);
+      }
+
+      // Save target ID to memory
+      creep.memory.target = target.length > 0 ? target[0].id : false;
     }
 
-    if(creep.memory.repairing) {
-      let target = creep.room.find(FIND_MY_STRUCTURES).filter(
-        object => object.hits < object.hitsMax
-      ).sort((a, b) => {
-        return a.hits - b.hits;
-      });
-      if(target.length === 0) {
-        target = creep.room.find(FIND_STRUCTURES).filter(
-          object => {
-            return (
-              (
-                object instanceof StructureWall ||
-                object instanceof StructureRampart ||
-                object instanceof StructureRoad
-              ) && object.hits < object.hitsMax
-            );
-          }
-        ).sort((a, b) => {
-          return a.hits - b.hits;
-        });
-      }
-      if(target.length > 0) {
-        const result = creep.repair(target[0]);
-        if(result == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target[0], {visualizePathStyle: {stroke: '#00ff00'}});
-        } else if(result !== 0) {
-          console.log('cannot repair: ' + result + ', ' + JSON.stringify(Object.values(target[0])));
-        }
+    // fetch target
+    const target = Game.getObjectById(creep.memory.target);
+    if(creep.memory.repairing && target) {
+      const result = creep.repair(target);
+      if(result == ERR_NOT_IN_RANGE) {
+        creep.moveTo(target, {visualizePathStyle: {stroke: '#00ff00'}});
+      } else if(result !== 0) {
+        console.log('cannot repair: ' + result + ', ' + JSON.stringify(Object.values(target)));
       }
     }
     else {
