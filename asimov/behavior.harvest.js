1808868cf95e403f32a84cc47f052fd2555276f4
thykka <thykka@gmail.com>
2019-05-26 13:13:29 +0300

    try again

--- asimov/behavior.harvest.js
+++ asimov/behavior.harvest.js
@@ -2,27 +2,25 @@ const harvestBehavior = function(creep, byPath = false) {
 
   let source = false;
 
-  source = creep.room.find(FIND_DROPPED_RESOURCES, {
+  sources = creep.room.find(FIND_DROPPED_RESOURCES, {
     filter: (o) => o.resourceType === RESOURCE_POWER || o instanceof Energy
   });
 
-  if(source.length && creep.pickup(source[0]) == ERR_NOT_IN_RANGE){
-    creep.moveTo(source[0].pos, { visualizePathStyle: {stroke: '#ffff00' }});
-    console.log('- ' + creep.name + ' pickup dropped #1/' + source.length);
-    return;
-  }
-
-  if(!source) {
+  if(!sources) {
     source = byPath ?
       creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE) :
       creep.room.find(FIND_SOURCES)[0];
   }
 
-  if(!source) {
+  if(!source && !sources.length) {
     console.log('- ' + creep.name + ': no harvest target');
   }
 
-  if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+  if(sources.length && creep.pickup(sources[0]) == ERR_NOT_IN_RANGE){
+    creep.moveTo(sources[0].pos, { visualizePathStyle: {stroke: '#ffff00' }});
+    console.log('- ' + creep.name + ' pickup dropped #1/' + sources.length);
+  }
+  else if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
     creep.moveTo(source, {visualizePathStyle: {stroke: '#ffff00'}});
   }
 };
