f310dae858ddcc4c25f0548aee42b9b7c769670d
thykka <thykka@gmail.com>
2019-05-26 14:50:30 +0300

    fix percentages

--- asimov/main.js
+++ asimov/main.js
@@ -44,6 +44,6 @@ module.exports.loop = function () {
     '> cache stats: w' +
     finder.cache.writes +
     '/r' + finder.cache.reads +
-    ' (' + (1 - (finder.cache.writes / finder.cache.reads)).toFixed(2) + '% utilization)'
+    ' (' + (1 - (finder.cache.writes * 100 / finder.cache.reads)).toFixed(2) + '% utilization)'
   );
 };
