9fbdb441142b3c9b148895d94754f6bfac0b3d7b
thykka <thykka@gmail.com>
2019-05-26 17:13:45 +0300

    sort totals?

--- asimov/util.display-totals.js
+++ asimov/util.display-totals.js
@@ -2,6 +2,7 @@ const countCreeps = require('util.count-creeps');
 
 module.exports = function displayTotals(ideals) {
   return Object.entries(countCreeps(Game.creeps))
+    .sort(([a], [b]) => a > b)
     .reduce((acc, [totalName, total], index, arr)=> {
       return acc + (
         totalName[0] + totalName[totalName.length-1] + ':' +
