31849e55ddf16c7f4c791b145faa79894b0bb3f2
thykka <thykka@gmail.com>
2019-05-26 15:42:36 +0300

    nerf bodies

--- asimov/settings.js
+++ asimov/settings.js
@@ -1,10 +1,10 @@
 module.exports = {
   debugLevel: 0,
   body: {
-    harvest: [WORK, WORK, WORK, CARRY, MOVE, MOVE],
-    build: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-    upgrade: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
-    repair: [WORK, WORK, CARRY, CARRY, MOVE, MOVE],
+    harvest: [WORK, WORK, CARRY, MOVE, MOVE],
+    build: [WORK, CARRY, CARRY, MOVE, MOVE],
+    upgrade: [WORK, CARRY, CARRY, MOVE, MOVE],
+    repair: [WORK, CARRY, CARRY, MOVE, MOVE],
   },
-  energyRequirement: 600,
+  energyRequirement: 400,
 };
