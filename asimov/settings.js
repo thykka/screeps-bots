4f0645afef0451af050a5072e03563e7786d113d
thykka <thykka@gmail.com>
2019-05-26 21:29:31 +0300

    levels settings

--- asimov/settings.js
+++ asimov/settings.js
@@ -6,27 +6,27 @@ const bodyLevels = {
     repair: [WORK, CARRY, MOVE],
   },
   2: {
+    harvest: [WORK, CARRY, MOVE],
+    build: [WORK, CARRY, MOVE],
+    upgrade: [WORK, CARRY, MOVE],
+    repair: [WORK, CARRY, MOVE],
+  },
+  3: {
     harvest: [WORK, WORK, CARRY, MOVE],
     build: [WORK, CARRY, CARRY, MOVE],
     upgrade: [WORK, CARRY, CARRY, MOVE],
     repair: [WORK, CARRY, CARRY, MOVE],
   },
-  3: {
-    harvest: [WORK, WORK, CARRY, MOVE, MOVE],
-    build: [WORK, CARRY, CARRY, MOVE, MOVE],
-    upgrade: [WORK, CARRY, CARRY, MOVE, MOVE],
-    repair: [WORK, CARRY, CARRY, MOVE, MOVE],
-  },
 };
 bodyLevels[0] = bodyLevels[1];
 bodyLevels[4] = bodyLevels[3];
 const energyLevels = {
-  0: 300,
   1: 300,
   2: 375,
   3: 450,
-  4: 450,
 };
+energyLevels[0] = energyLevels[1];
+energyLevels[4] = energyLevels[3];
 
 module.exports = {
   debugLevel: 1,
