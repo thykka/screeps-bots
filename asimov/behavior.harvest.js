13ccef310886d923ee9b9e75011ec8b355ef2028
thykka <thykka@gmail.com>
2019-05-26 13:15:02 +0300

    ifff

--- asimov/behavior.harvest.js
+++ asimov/behavior.harvest.js
@@ -6,7 +6,7 @@ const harvestBehavior = function(creep, byPath = false) {
     filter: (o) => o.resourceType === RESOURCE_POWER || o instanceof Energy
   });
 
-  if(!sources) {
+  if(sources.length === 0) {
     source = byPath ?
       creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE) :
       creep.room.find(FIND_SOURCES)[0];
@@ -16,11 +16,11 @@ const harvestBehavior = function(creep, byPath = false) {
     console.log('- ' + creep.name + ': no harvest target');
   }
 
-  if(sources.length && creep.pickup(sources[0]) == ERR_NOT_IN_RANGE){
+  if(sources.length > 0 && creep.pickup(sources[0]) == ERR_NOT_IN_RANGE){
     creep.moveTo(sources[0].pos, { visualizePathStyle: {stroke: '#ffff00' }});
     console.log('- ' + creep.name + ' pickup dropped #1/' + sources.length);
   }
-  else if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+  if(sources.length === 0 && creep.harvest(source) == ERR_NOT_IN_RANGE) {
     creep.moveTo(source, {visualizePathStyle: {stroke: '#ffff00'}});
   }
 };
