7a5ab6f96196bfd26ea3e49bcad04bc845f6de31
thykka <thykka@gmail.com>
2019-05-27 21:49:07 +0300

    +spawn a creep if < 1, +prototype shorthand

--- babbage/spawn.js
+++ babbage/spawn.js
@@ -1,17 +1,41 @@
 StructureSpawn.prototype.getRoomEnergy = function getRoomEnergy(extensions) {
   return this.energy + extensions.reduce((acc, o) => o.energy + acc, 0);
 };
+StructureSpawn.prototype.newCreep = function newCreep(opts) {
+  const defs = {
+    body: [WORK, CARRY, MOVE],
+    name: 'c0',
+    task: false,
+    home: this.room.name,
+  };
+  const c = Object.assign({}, defs, opts);
+
+  const result = this.spawnCreep(c.body, c.name, {
+    memory: {
+      home: c.home,
+      task: c.task,
+    }
+  });
+
+  if(!result) {
+    return Game.creeps[c.name];
+  }
+  console.log(this.name + ': Spawn ' + c.name + ' failed: ' + result);
+};
 
 module.exports.loop = function loopSpawn(opts) {
   _.forEach(Game.spawns, (spawn => {
-
-    const roomExtensions = this.room.find(FIND_MY_STRUCTURES, {
+    const roomExtensions = spawn.room.find(FIND_MY_STRUCTURES, {
       filter: o => o instanceof StructureExtension
     });
 
-    console.log(
-      spawn.name + ': ' +
-      spawn.getRoomEnergy(roomExtensions)
-    );
+    const energy = spawn.getRoomEnergy;
+    const minEnergyToSpawn = 300;
+
+    if(energy > minEnergyToSpawn && _.size(Game.creeps) === 0) {
+      spawn.newCreep();
+    }
+
+    console.log(spawn.name + ' E' + energy);
   }));
 };
