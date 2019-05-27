6d7c6f87196cba101aee88da284f338617f6bb98
thykka <thykka@gmail.com>
2019-05-28 02:10:03 +0300

    add memory cleaner

--- babbage/main.js
+++ babbage/main.js
@@ -7,9 +7,22 @@ const loopModules = [
 loopModules.forEach(module => loops[module] = require(module));
 
 module.exports.loop = function mainLoop() {
+  cleanMemory();
+
   loopModules.forEach(module => {
     try {
       const result = loops[module].loop();
     } catch(e) { console.log(e); }
   });
+};
+
+function cleanMemory() {
+  let dead = false;
+  for(var name in Memory.creeps) {
+    if(!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      dead = !dead ? name : dead + ', ' + name;
+    }
+  }
+  return dead;
 };
