17faa776f88e48e3354fdb91bbb041fa954c8194
thykka <thykka@gmail.com>
2019-05-27 22:43:15 +0300

    hmm?

--- babbage/creep.js
+++ babbage/creep.js
@@ -32,6 +32,7 @@ module.exports.loop = function loopCreep(opts) {
 
   for(const creepName in myCreeps) {
     const creep = myCreeps[creepName];
+    const task = creep.memory.task;
 
     if(creep.ticksToLive < returnThreshold) { // creep about to expire
       console.log(creep.name + ' expiring: ' + creep.ticksToLive);
