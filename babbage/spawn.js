921ad97042c52583e7735f0a8400fefc2f0e44c6
thykka <thykka@gmail.com>
2019-05-27 21:04:47 +0300

    basic helloworld

--- babbage/spawn.js
+++ babbage/spawn.js
@@ -1,6 +1,10 @@
 module.exports.loop = function loopSpawn(opts) {
   const cache = opts.cache || {};
+  _.forEach(Game.spawns, (spawn => {
 
+    console.log('Hello, ' + spawn.name + '!');
+
+  }));
   return {
     cache
   };
