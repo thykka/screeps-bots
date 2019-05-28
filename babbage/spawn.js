0a2d09215f69e911d201e53f0cd2f806afe64456
thykka <thykka@gmail.com>
2019-05-28 22:21:04 +0300

    spawn by RCL

--- babbage/spawn.js
+++ babbage/spawn.js
@@ -1,5 +1,4 @@
 const Renew = require('renew');
-
 module.exports.loop = function loopSpawn(opts) {
   _.forEach(Game.spawns, (spawn => {
     const roomExtensions = spawn.room.find(FIND_MY_STRUCTURES, {
@@ -19,21 +18,41 @@ module.exports.loop = function loopSpawn(opts) {
       spawn.renewCreeps();
 
       if(energy >= minEnergyToSpawn) {
-        // spawn stuff maybe?
+        const creepCount = _.size(Game.creeps);
+        const currentLevel = spawn.room.controller.level;
+        const idealCount = SpawnLevel[currentLevel].ideal;
+
+        if(creepCount < idealCount) {
+          spawn.newCreep({ level: currentLevel });
+        }
+
       } else if (Game.time % 1 === 0) {
-        const percentage = 100 * (energy / minEnergyToSpawn);
-        new RoomVisual(spawn.room).text(
-          energy + '/' + minEnergyToSpawn + ' ' + percentage.toFixed(0) + '%',
-          spawn.pos.x, spawn.pos.y + 1.2, {
-            color: '#FF0',
-            font: 0.3
-          }
-        );
+        spawn.visualizeEnergy(energy, minEnergyToSpawn);
       }
     }
   }));
 };
 
+const SpawnLevel = {
+  '0': {
+    ideal: 1,
+    body: [WORK, CARRY, MOVE],
+  },
+  '1': {
+    ideal: 5,
+    body: [WORK, CARRY, MOVE],
+  },
+  '2': {
+    ideal: 10,
+    body: [WORK, CARRY, MOVE],
+  },
+  '3': {
+    ideal: 15,
+    body: [WORK, CARRY, MOVE],
+  }
+};
+
+module.exports.SpawnBehavior = SpawnBehavior;
 StructureSpawn.prototype.renewCreeps = Renew.renewCreeps;
 
 StructureSpawn.prototype.getRoomEnergy = function getRoomEnergy(extensions) {
@@ -77,4 +96,13 @@ StructureSpawn.prototype.getAdjacentCreeps = function getAdjacentCreeps() {
       );
     }
   });
+};
+
+StructureSpawn.prototype.visualizeEnergy = function visualizeEnergy(energy = -1, minEnergy = 1) {
+  const percentage = 100 * (energy / minEnergy);
+  new RoomVisual(this.room).text(
+    `${ energy }/${ minEnergy } ${ percentage.toFixed(0) }%`,
+    this.pos.x, this.pos.y + 1.2,
+    { font: 0.3, color: '#FF0', }
+  );
 };
