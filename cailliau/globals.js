2249b1a1737b9bd1752a063dc74cd5f032b70170
thykka <thykka@gmail.com>
2019-05-31 19:04:35 +0300

    documentation

--- cailliau/globals.js
+++ cailliau/globals.js
@@ -1,16 +1,39 @@
 
+/**
+ * Surround sources with containers for static mining
+ */
 Room.prototype.setupSources = function setupSources() {
   const roomSources = this.findSources();
+  const errors = [];
   _.forEach(roomSources, (source, sourceIndex) => {
     _.forEach(source.findContainerPositions(), (pos, posIndex) => {
-      this.createConstructionSite(pos.x, pos.y, STRUCTURE_CONTAINER, 'C' + sourceIndex + '' + posIndex);
+      errors.push(
+        this.createConstructionSite(pos.x, pos.y, STRUCTURE_CONTAINER, 'C' + sourceIndex + '' + posIndex)
+      );
     });
   });
 };
+/**
+ * Wrapper for Room.find(FIND_SOURCES)
+ * @returns {array|null} - results of Room.find, might be null if none found
+ */
 Room.prototype.findSources = function findSources() {
   return this.find(FIND_SOURCES);
 };
 
+/**
+ * Looks for places to put a static miner container onto, in the following figure:
+ *
+ * ..C..
+ * .....
+ * C.S.C
+ * .....
+ * ..C..
+ *
+ * ...where "S" represents the source position, and "C" the positions that
+ * are checked for construction site placement.
+ * @returns {array} - Array of suitable construction site positions
+ */
 Source.prototype.findContainerPositions = function findContainerPositions() {
   return [
     new RoomPosition(this.pos.x - 2, this.pos.y, this.room.name),
